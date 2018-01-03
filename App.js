f4bb7b545f487e40daf00bd7c8ee72788cd17833
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2018-01-03 15:42:46 +0200

    Two components created for Search and Table

--- App.js
+++ App.js
@@ -59,42 +59,96 @@ class App extends Component {
   render() {
     const helloWorld = 'Welcome to the Road to learn React';
     var person = {firstName: "Tadas", lastName: "Davidsonas"};
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        <p>
-          The user learning this course is {person.firstName} {person.lastName}<br/>
-          module.hot helps to make refresh without reloading the page
-        </p>
+        <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+        />
+        <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+        />
 
-          <form>
-              <input
-                  type="text"
-                  onChange={this.onSearchChange}
-              />
-          </form>
+        {/*<h2>{helloWorld}</h2>*/}
+        {/*<p>*/}
+          {/*The user learning this course is {person.firstName} {person.lastName}<br/>*/}
+          {/*module.hot helps to make refresh without reloading the page*/}
+        {/*</p>*/}
+          {/*<form>*/}
+              {/*<input*/}
+                  {/*type="text"*/}
+                  {/*value={searchTerm}*/}
+                  {/*onChange={this.onSearchChange}*/}
+              {/*/>*/}
+          {/*</form>*/}
 
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                    onClick={() => this.onDismiss(item.objectID)}
-                    type="button">
-                        Dismiss
-                </button>
-              </span>
-            </div>
-        )}
+        {/*{this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>*/}
+            {/*<div key={item.objectID}>*/}
+              {/*<span>*/}
+                {/*<a href={item.url}>{item.title}</a>*/}
+              {/*</span>*/}
+              {/*<span>{item.author}</span>*/}
+              {/*<span>{item.num_comments}</span>*/}
+              {/*<span>{item.points}</span>*/}
+              {/*<span>*/}
+                {/*<button*/}
+                    {/*onClick={() => this.onDismiss(item.objectID)}*/}
+                    {/*type="button">*/}
+                        {/*Dismiss*/}
+                {/*</button>*/}
+              {/*</span>*/}
+            {/*</div>*/}
+        {/*)}*/}
 
       </div>
     );
   }
+}
+
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const {list, pattern, onDismiss} = this.props;
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
+                    <div key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <button
+                                onClick={() => onDismiss(item.objectID)}
+                                type="button"
+                            >
+                                Dismiss
+                            </button>
+                        </span>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default App;
